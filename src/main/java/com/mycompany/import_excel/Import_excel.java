9dc3080e74188890938ee94fab82be89b47a7122
jerbi2026 <jerbiahmed24@gmail.com>
2024-06-24 17:13:36 +0100

    application java

---
+++ src/main/java/com/mycompany/import_excel/Import_excel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.import_excel;
+
+/**
+ *
+ * @author jerbi
+ */
+public class Import_excel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/import_excel/fichier_excel.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.import_excel;
+
+import java.io.File;
+
+/**
+ *
+ * @author jerbi
+ */
+public class fichier_excel {
+    File excel;
+}
